6cf8bc686e05a316f3fb60c205eaa1478dd64d11
Gabriel Silva <00001122947975sp@al.educacao.sp.gov.br>
2026-06-23 21:20:49 -0300

    Update
    Coloquei pra imagem aparecer ao apertar o bad-btt;
    
    Diminui o tempo do timeout de 20s para 1s;
    
    Aumentei a escala da imagem, adicionando um scale: 3;
    
    Removi o coisa que fazia o texto ir pro primeiro item do dicionário toda vez que a imagem sumia;
    
    Adicionei uma nova área pra imagens;
    
    Adicionei mais imagens e fiz com que aparecessem de forma aleatória, sem repetir a anterior;

--- script.js
+++ script.js
@@ -20,7 +20,24 @@ const chaves = Object.keys(dicionario);
 
 let indice = 0;
 
-// ==================== FUNÇÕES ====================
+// ==================== IMAGENS =======================
+
+//var pra pegar o id da imagem-central
+const imagemCentral = document.getElementById("imagem-central");
+
+const imagens = [
+    "/imgs/therock.jpg",
+    "/imgs/serio-neguinho.png",
+    "/imgs/realistic-sob-crying-emoji.png"
+];
+
+//var pra armazenar a img_sorteada
+let img_sorteada;
+
+//var pra guardar a ultima imagem que foi sorteada
+let ultimaImagem = null;
+
+// ==================== FUNÇÕES =======================
 
 function atualizarTexto() {
     indice++;
@@ -48,14 +65,6 @@ function tocarMusica() {
 function mensagemEspecial() {
     paragrafo.textContent = dicionario.special;
 
-    // Mostra a imagem
-    janelaImagem.classList.remove("escondido");
-
-    // Esconde depois de 3 segundos
-    setTimeout(() => {
-        janelaImagem.classList.add("escondido");
-        paragrafo.textContent = dicionario.primeiro;
-    }, 20000);
 }
 
 // ==================== EVENTOS ====================
@@ -65,7 +74,35 @@ goodBtt.addEventListener("click", () => {
     tocarMusica();
 });
 
+
 badBtt.addEventListener("click", () => {
     atualizarTexto();
     tocarImpacto();
+
+    // Sorteia até pegar uma diferente da última
+    do 
+    {
+        //pega um índice aleatório entre 0 e o tamanho do array 
+        //math.floor arrendonda e transforma em n° inteiro
+        img_sorteada = imagens[Math.floor(Math.random() * imagens.length)];
+    } 
+
+    //repete o sorteio se cair na mesma da última vez
+    while (img_sorteada === ultimaImagem);
+    
+    //salva a imagem sorteada como "última"
+    ultimaImagem = img_sorteada;
+
+    //troca o scr da imagem do meu html pelo scr da imagem sorteada
+    imagemCentral.src = img_sorteada;
+
+    //Mostra a imagem
+    janelaImagem.classList.remove("escondido");
+
+    //Esconde depois de 1 segundos
+    setTimeout(() => {
+        janelaImagem.classList.add("escondido");
+        //paragrafo.textContent = dicionario.primeiro;
+    }, 1000);
+
 });
